23e2732172dc8d2a292f0113c244af438c7bdd93
Jaspreet SIngh <jaskaura27@gmail.com>
2021-03-09 21:18:18 -0800

    Update and rename Lab08.cpp to BracketStringMatch.cpp

--- BracketStringMatch.cpp
+++ BracketStringMatch.cpp
@@ -1,5 +1,4 @@
-// Lab08.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//Jaspreet Singh (100345381)
+
 /*
 	this program check if a string of brackets is a match or not using two different algorithms one with vector stack and other with list stack.
 	there are total six test case three for each.
@@ -45,14 +44,3 @@ int main()
 	test(c.isMatched(), "(((){})");
 	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
